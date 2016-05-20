1ffc7cbd4705054741516ac28fa4c4f15d02d2b4
WarmColor <admin@warmcolor.net>
2016-05-20 15:59:18 +0800

    edit activity_my.xml Intent

--- app/src/main/java/net/warmcolor/blog/myfirstapp/MyActivity.java
+++ app/src/main/java/net/warmcolor/blog/myfirstapp/MyActivity.java
@@ -1,5 +1,7 @@
 package net.warmcolor.blog.myfirstapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSGE = "net.warmcolor.blog.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +46,24 @@ public class MyActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSGE, message);
+        startActivity(intent);
+
+    }
+
 }
