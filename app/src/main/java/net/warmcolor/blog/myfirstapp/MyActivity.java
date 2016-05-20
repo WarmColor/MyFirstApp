203ab99a82f0b197a35bc0b7a3526851a9261e20
WarmColor <admin@warmcolor.net>
2016-05-20 16:29:18 +0800

    edit DisplayMessageActivity

--- app/src/main/java/net/warmcolor/blog/myfirstapp/MyActivity.java
+++ app/src/main/java/net/warmcolor/blog/myfirstapp/MyActivity.java
@@ -1,15 +1,11 @@
 package net.warmcolor.blog.myfirstapp;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
